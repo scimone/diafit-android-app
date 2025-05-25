c3459169d3203a9a9a6510db3faece766333d0dd
Anna Scimone <56034784+scimone@users.noreply.github.com>
2025-05-25 18:25:55 +0200

    Square images with round corners

--- app/src/main/java/uk/scimone/diafit/journal/presentation/components/JournalItem.kt
+++ app/src/main/java/uk/scimone/diafit/journal/presentation/components/JournalItem.kt
@@ -2,16 +2,20 @@ package uk.scimone.diafit.journal.presentation.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import uk.scimone.diafit.core.domain.model.MealEntity
 import uk.scimone.diafit.journal.presentation.model.toUi
+import androidx.compose.ui.layout.ContentScale
+
 
 @Composable
 fun JournalItem(meal: MealEntity) {
@@ -21,19 +25,21 @@ fun JournalItem(meal: MealEntity) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(12.dp),
+            .padding(8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         mealUi.imageUri?.let {
             Image(
                 painter = rememberAsyncImagePainter(it),
                 contentDescription = "Meal Image",
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .size(80.dp)
-                    .padding(end = 16.dp)
+                    .width(80.dp)
+                    .aspectRatio(1f)
+                    .padding(5.dp)
+                    .clip(RoundedCornerShape(12.dp))  // Add rounded corners here
             )
         }
-
         Column(modifier = Modifier.weight(1f)) {
             Text(text = "Time: ${mealUi.timeFormatted}", style = MaterialTheme.typography.bodyMedium)
             Text(text = mealUi.description, style = MaterialTheme.typography.bodyLarge)
