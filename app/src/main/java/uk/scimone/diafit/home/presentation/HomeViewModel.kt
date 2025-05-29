795f04519e31a2ae0bd0a405fbb5f03e89ba26bf
Anna Scimone <56034784+scimone@users.noreply.github.com>
2025-05-29 20:21:07 +0200

    Live ticking timer

--- app/src/main/java/uk/scimone/diafit/home/presentation/HomeViewModel.kt
+++ app/src/main/java/uk/scimone/diafit/home/presentation/HomeViewModel.kt
@@ -2,40 +2,54 @@ package uk.scimone.diafit.home.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import uk.scimone.diafit.core.domain.repository.CgmRepository
 import uk.scimone.diafit.home.presentation.model.toCgmEntityUi
+import uk.scimone.diafit.core.domain.model.CgmEntity
 
 class HomeViewModel(
     private val cgmRepository: CgmRepository,
     private val userId: Int,
 ) : ViewModel() {
 
+    private val latestCgm = MutableStateFlow<CgmEntity?>(null)
+
     private val _state = MutableStateFlow(HomeState())
     val state: StateFlow<HomeState> = _state.asStateFlow()
 
     init {
         observeLatestCgm()
+        startTicker()
     }
 
     private fun observeLatestCgm() {
         viewModelScope.launch {
             cgmRepository.getLatestCgm()
-                .map { cgm -> cgm.toCgmEntityUi() }
-                .onEach { uiModel ->
-                    _state.value = HomeState(
-                        cgmUi = uiModel,
-                        isLoading = false
-                    )
-                }
                 .catch { e ->
                     _state.value = HomeState(
                         error = e.message,
                         isLoading = false
                     )
                 }
-                .collect()
+                .collect { cgm ->
+                    latestCgm.value = cgm
+                }
+        }
+    }
+
+    private fun startTicker() {
+        viewModelScope.launch {
+            while (true) {
+                latestCgm.value?.let { cgm ->
+                    _state.value = HomeState(
+                        cgmUi = cgm.toCgmEntityUi(),
+                        isLoading = false
+                    )
+                }
+                delay(1000L) // Tick every second
+            }
         }
     }
 }
